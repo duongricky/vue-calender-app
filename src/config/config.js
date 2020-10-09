191d24cbd50f726117ae880b3a741b9bf68e33c3
duongnva-2181 <nguyen.van.anh.duong@sun-asterisk.com>
2020-10-09 09:19:41 +0700

    config firebase

---
+++ src/config/config.js
@@ -0,0 +1,13 @@
+const config = {
+  firebaseConfig: {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+  }
+}
+
+export default { config }
